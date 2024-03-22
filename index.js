e3b1247fa8ab5bda4ff5cdfd19f7e87961ae5422
Alexandre78R <alexandre.renard98@gmail.com>
2024-03-22 09:27:13 +0100

    push exo

---
+++ index.js
@@ -0,0 +1,24 @@
+const data = require("./data.json");
+
+let statutConnect = false;
+let user = {};
+
+//Function de connexion
+const connect = (pseudo, password) => {
+}
+
+connect("Alexandre", "alexandre");
+
+
+//Function de Zone xp lvl 10 min (sinon message d'alert DANGER)
+const zoneXp10Min = () => {
+
+}
+
+zoneXp10Min();
+
+//Function Admin - searchUser
+const searchUser = (pseudo) => {
+}
+
+searchUser("nico")
